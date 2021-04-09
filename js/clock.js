3ff04f5a56cfc22d63aa075aa73828c791c3ef80
facundonapal <indiecitus@gmail.com>
2021-04-09 09:31:25 -0300

    Add: .pdf cv updates

--- js/clock.js
+++ js/clock.js
@@ -13,6 +13,10 @@ function engineClock(){
     if (m < 10) {
         m = "0" + m;
     }
+
+    if (h < 10) {
+        h = "0" + h;
+    }
     
     let outputTime = h + " : " + m + " : " + s;
     
